4a491ed85c5290827d1f71bcda0865b3e6993a40
parkjunghoon <go9018@naver.com>
2020-09-15 19:09:40 +0900

    add : location permission check tedpermission library

--- mask_app_java/src/main/java/junghoon/jetpack/sample/mask_app_java/MainActivity.java
+++ mask_app_java/src/main/java/junghoon/jetpack/sample/mask_app_java/MainActivity.java
@@ -1,5 +1,8 @@
 package junghoon.jetpack.sample.mask_app_java;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,16 +14,61 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
+
 import junghoon.jetpack.sample.mask_app_java.adapter.StoreAdapter;
 import junghoon.jetpack.sample.mask_app_java.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                performAction();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
+        performAction();
+        viewModel.fetchStoreInfo();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void performAction() {
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                    }
+                });
 
         RecyclerView recyclerView = findViewById(R.id.store_recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
@@ -34,8 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 getSupportActionBar().setTitle("마스크 재고 있는 곳 : "+ stores.size());
             }
         });
-
-        viewModel.fetchStoreInfo();
     }
 
     @Override
