3c47ea937e32d06ed1ff4e562fa9199c1e9d46c4
parkjunghoon <go9018@naver.com>
2020-09-28 17:54:24 +0900

    add : setFragmentResult, setFragmentResultListener 를 사용한 데이터 전달 예제

--- data_transfer/src/main/java/junghoon/jetpack/sample/data_transfer/FirstFragment.kt
+++ data_transfer/src/main/java/junghoon/jetpack/sample/data_transfer/FirstFragment.kt
@@ -1,25 +1,26 @@
 package junghoon.jetpack.sample.data_transfer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.core.os.bundleOf
+import androidx.fragment.app.*
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 //onCreateView 메서드 오버라이드 안하고 위처럼 생성자로 뷰 넘겨줘도됨
 
-    private val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         button.setOnClickListener {
-            mainViewModel.data = "전달하는 데이터"
+
+            //데이터를 보낼때
+            setFragmentResult("firstToSecond", bundleOf("data" to "전달하는 데이터 "))
+
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
--- data_transfer/src/main/java/junghoon/jetpack/sample/data_transfer/SecondFragment.kt
+++ data_transfer/src/main/java/junghoon/jetpack/sample/data_transfer/SecondFragment.kt
@@ -7,21 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
 //onCreateView 메서드 오버라이드 안하고 위처럼 생성자로 뷰 넘겨줘도됨
 
-    private val mainViewModel by activityViewModels<MainViewModel>()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_SHORT).show()
-
         button2.setOnClickListener {
+
+            //데이터를 받을때
+            setFragmentResultListener("firstToSecond") { _, bundle ->
+                val data = bundle.getString("data","")
+                Toast.makeText(requireContext(), data, Toast.LENGTH_SHORT).show()
+            }
+
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+
         }
     }
 }
