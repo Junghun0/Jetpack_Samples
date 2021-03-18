d24d50cbec4692ea44486bac837e41a252a204db
parkjunghoon <go9018@naver.com>
2021-03-18 19:36:58 +0900

    add  : repo recyclerview item / main xml

--- paging_sample_app/src/main/java/com/fishing/paging_sample_app/MainActivity.kt
+++ paging_sample_app/src/main/java/com/fishing/paging_sample_app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.fishing.paging_sample_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fishing.paging_sample_app.ui.SearchRepositoriesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: SearchRepositoriesViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ paging_sample_app/src/main/java/com/fishing/paging_sample_app/ui/SearchRepositoriesViewModel.kt
@@ -0,0 +1,8 @@
+package com.fishing.paging_sample_app.ui
+
+import androidx.lifecycle.ViewModel
+
+class SearchRepositoriesViewModel : ViewModel() {
+
+
+}
